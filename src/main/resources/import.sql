b9a889ad6eb8bcc63c7896365ec4e67e424bf7ca
jamesrloveday <jamesrloveday@outlook.com>
2016-07-07 13:16:39 +0100

    Altered the sql file
    Altered the sql file renaming from data to import in line with spring boot
    expectations, removed the create table file as it's not needed.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO IMAGE (ID, FILE_LOCATION, FORM, SECTION_NAME, TITLE) VALUES (1, 'C:\Users\james\Documents\java projects\Wing-Chun-Web-App-JSP\src\main\webapp\resources\images\siuNimTao\basic stance.jpg', 'siu nim tao', 'section 1', 'basic stance')
+INSERT INTO IMAGE (ID, FILE_LOCATION, FORM, SECTION_NAME, TITLE) VALUES (2, 'C:\Users\james\Documents\java projects\Wing-Chun-Web-App-JSP\src\main\webapp\resources\images\siuNimTao\jic kuen.png', 'siu nim tao', 'section 1', 'jic kuen')
+INSERT INTO IMAGE (ID, FILE_LOCATION, FORM, SECTION_NAME, TITLE) VALUES (3, 'C:\Users\james\Documents\java projects\Wing-Chun-Web-App-JSP\src\main\webapp\resources\images\siuNimTao\jic kuen 1.png', 'siu nim tao', 'section 1', 'jic kuen')
+
